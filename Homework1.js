a34922db1494fee96ef2a9cd3008b546253b7294
AnastasiiaYaM <106627623+AnastasiiaYaM@users.noreply.github.com>
2023-07-15 18:22:45 +0300

    Multiplication added

--- Homework1.js
+++ Homework1.js
@@ -43,7 +43,27 @@ return `${res}`;
 console.log('substraction result fn', substractFn("99888777998877889998878899987898778", "99888777998877889998878899987898770"));
 
 
+// 4th task - Multiplication
 
+const multiplyFn = (firstNum, secNum) => {
+  if (firstNum === '0' || secNum === '0') return '0'
+  
+  const m = firstNum.length, n = secNum.length, res = new Array(m+n).fill(0);
+  
+  for (let i=m-1; i>=0; i--) {
+      for (let j=n-1; j>=0; j--) {
+          const p1=i+j, p2=i+j+1;
+          let sum = res[p2] + Number(firstNum[i]) * Number(secNum[j]);
+          res[p2] = sum % 10;
+          res[p1] += Math.floor(sum / 10);
+      }
+  }
+  if (res[0] === 0) res.shift();
+  return res.join('');
+  
+}
+
+console.log('multiplication result fn', multiplyFn("99888777998877889998878899987898778", "99888777998877889998878899987898770"));
 
 
 /* 
@@ -62,7 +82,7 @@ const sumArrEl = (accumulator, number) => accumulator + number;
 console.log(arrOfAbsNum.reduce(sumArrEl).toString());
 
 
-// 2nd task - Subtraction
+// 2nd task - Substraction
 
 const subtractArrEl = (accumulator, number) =>  accumulator - number;
 
