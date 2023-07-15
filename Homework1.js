f84756e1ff1370396512c7e7300dd72fc2bf73bf
AnastasiiaYaM <106627623+AnastasiiaYaM@users.noreply.github.com>
2023-07-15 17:29:13 +0300

    Updated algorithm for adding strings of two large numbers
    const sumFn = (firstNum, secNum) =>

--- Homework1.js
+++ Homework1.js
@@ -1,3 +1,29 @@
+
+// 1st task - SUM 
+
+const sumFn = (firstNum, secNum) => {
+  let i = firstNum.length - 1;
+  let j = secNum.length - 1;
+  let sum = 0, carryover = 0, res = ' ';
+  while (i >= 0 || j >= 0) {
+    const x = firstNum[i] || 0;
+    const y = secNum[j] || 0;
+    sum = parseInt(x) + parseInt(y) + carryover;
+    carryover = Math.trunc(sum / 10);
+    res = (sum % 10) + res;
+    i--;
+    j--;
+  }
+if (carryover) res = carryover + res;
+return `${res}`;
+}
+
+console.log('sum result fn', sumFn("99888777998877889998878899987898778", "99888777998877889998878899987898778"));
+
+
+
+
+/* 
 let newString = '99888777998877889998878899987898778';
 
 const arrOfNum = newString.split('');
@@ -41,3 +67,4 @@ console.log(arrOfNumDivideByTwoArrEl.toString());
 const multiplyByTwoArrEl = arrOfAbsNum.map(x => x * 2); 
 
 console.log(multiplyByTwoArrEl.toString());
+*/
