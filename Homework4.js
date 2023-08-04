16d54cb233a2d5ac197cebce0a05d4fbf316bdbe
AnastasiiaYaM <106627623+AnastasiiaYaM@users.noreply.github.com>
2023-08-04 16:36:15 +0300

    HW 4, the 6th task

--- Homework4.js
+++ Homework4.js
@@ -224,7 +224,38 @@ observeObject.age = 44;
 /* Task 6: Object Deep Cloning
 
 Implement a function called deepCloneObject that takes an object as an argument and returns a deep copy of the object. The function should handle circular references and complex nested structures. Do not use JSON methods.
-/*
+*/
+
+function deepCloneObject(obj) {
+
+    if (Array.isArray(obj)) {
+
+      return obj.map((item) => deepCloneObject(item));
+
+    } 
+    
+    else if (typeof obj === 'object' && obj !== null) {
+
+      const clone = {};
+
+      for (let key in obj) {
+
+        if (obj.hasOwnProperty(key)) {
+
+          clone[key] = deepCloneObject(obj[key]);
+
+        }
+      }
+
+      return clone;
+
+    } else {
+
+      return obj;
+    }
+  }
+
+console.log('deep clone object', deepCloneObject(person));
 
 
 /* Task 7: Object Property Validation
