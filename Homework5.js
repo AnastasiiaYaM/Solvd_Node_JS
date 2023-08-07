2eaeb447a7d1b430adaaeb8cac6338e72ee35a91
AnastasiiaYaM <106627623+AnastasiiaYaM@users.noreply.github.com>
2023-08-07 13:15:22 +0300

    chunkArray Fn
    var to let - fixed

--- Homework5.js
+++ Homework5.js
@@ -91,7 +91,7 @@ function chunkArray(arr, size) {
 
     var myArray = [];
 
-    for(var i = 0; i < arr.length; i += size) {
+    for(let i = 0; i < arr.length; i += size) {
 
       myArray.push(arr.slice(i, i+size));
       
